94d2ea3a0af23e29982039950b36dfcd6240675c
chae03yb <chae03yb@gmail.com>
2021-01-12 17:51:30 +0900

    update http.createServer

--- Server/main.js
+++ Server/main.js
@@ -3,7 +3,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    
+    if (req.method === "GET" && req.url.startsWith("/log/")) {
+        // get log
+        console.log(req.url + "GET");
+    }
+    else if (req.method === "POST" && req.url.startsWith("/log/")) {
+        // update log
+        console.log(req.url + "POST");
+    }
+    else {
+        console.log(req.url);
+    }
 });
 
 server.listen(8956, "localhost", () => {
